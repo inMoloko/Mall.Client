b679cab62b89bfc9e079bb25ddda0f6dbf627cbe
nekrasovinmoloko <pitchcontrol@yandex.ru>
2017-05-04 15:21:26 +0500

    Отломал обратный отчет

--- Scripts/idleConfig.js
+++ Scripts/idleConfig.js
@@ -1,13 +1,14 @@
 ﻿(function () {
     "use strict";
     var app = angular.module('app');
-    app.config(function(IdleProvider, KeepaliveProvider) {
+    app.config(function (IdleProvider, KeepaliveProvider, TitleProvider) {
         // configure Idle settings
         IdleProvider.idle(30); // in seconds
         IdleProvider.timeout(30); // in seconds
         KeepaliveProvider.interval(30); // in seconds
+        TitleProvider.enabled(false);
     })
-    .run(function(Idle){
-        Idle.watch();
-    });
+        .run(function (Idle) {
+            Idle.watch();
+        });
 })();
